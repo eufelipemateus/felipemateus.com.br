7a6653b9e5e44ee541251b7a9eda969fb3f929ee
Felipe Mateus <eu@felipemateus.com>
2026-02-07 21:49:03 -0300

    update site

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./**/*.html",
+    "./src/**/*.css",
+  ],
+  safelist: [
+
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        primary: '#6366f1',
+      },
+    },
+  },
+  plugins: [],
+}
